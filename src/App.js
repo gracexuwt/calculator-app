c9fdc7f42e20e216041b90adff70a58932e14e6e
Grace Xu <92956740+gracexuwt@users.noreply.github.com>
2023-01-17 19:25:02 -0500

    Alternative to eval()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 
+
 const App = () => {
     const [result, setResult] = useState("");
     
-    const handleClick = (e) => {
+    const handle = (e) => {
         setResult(result.concat(e.target.name));
 
     }
@@ -16,12 +17,16 @@ const App = () => {
     }
     const calculate = () => {
         try {
-            setResult(eval(result).toString());
+            setResult(evil(result).toString());
         }
         catch(err){ // When mutliple symbols are lined up
             setResult("Error")
         }
     }
+    function evil(fn) {
+        return new Function('return ' + fn)();
+    }
+
     return(
         <div className="wrapper">
             <form>
@@ -31,21 +36,21 @@ const App = () => {
             <div className="keypad">
                 <button class="highlight" onClick={clear} id="clear">Clear</button>
                 <button class="highlight" onClick={back} id="back">C</button>
-                <button class="highlight" name="/" onClick={handleClick}>&divide;</button>
-                <button name="7" onClick={handleClick}>7</button>
-                <button name="8" onClick={handleClick}>8</button>
-                <button name="9" onClick={handleClick}>9</button>
-                <button class="highlight" name="*" onClick={handleClick}>&times;</button>
-                <button name="4" onClick={handleClick}>4</button>
-                <button name="5" onClick={handleClick}>5</button>
-                <button name="6" onClick={handleClick}>6</button>
-                <button class="highlight" name="-" onClick={handleClick}>&ndash;</button>
-                <button name="1" onClick={handleClick}>1</button>
-                <button name="2" onClick={handleClick}>2</button>
-                <button name="3" onClick={handleClick}>3</button>
-                <button class="highlight" name="+" onClick={handleClick}>+</button>
-                <button name="0" onClick={handleClick}>0</button>
-                <button name="." onClick={handleClick}>.</button>
+                <button class="highlight" name="/" onClick={handle}>&divide;</button>
+                <button name="7" onClick={handle}>7</button>
+                <button name="8" onClick={handle}>8</button>
+                <button name="9" onClick={handle}>9</button>
+                <button class="highlight" name="*" onClick={handle}>&times;</button>
+                <button name="4" onClick={handle}>4</button>
+                <button name="5" onClick={handle}>5</button>
+                <button name="6" onClick={handle}>6</button>
+                <button class="highlight" name="-" onClick={handle}>&ndash;</button>
+                <button name="1" onClick={handle}>1</button>
+                <button name="2" onClick={handle}>2</button>
+                <button name="3" onClick={handle}>3</button>
+                <button class="highlight" name="+" onClick={handle}>+</button>
+                <button name="0" onClick={handle}>0</button>
+                <button name="." onClick={handle}>.</button>
                 <button class="highlight" onClick={calculate} id="result">=</button>
             </div>
         </div>
